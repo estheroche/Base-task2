0f75a259503518e8de49809e73b6b31b85a13dca
estherroche <estheraladioche569@gmail.com>
2025-10-25 08:47:43 -0500

    add farcaster mini app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { sdk } from "@farcaster/miniapp-sdk";
 import "./App.css";
 import WalletConnector from "./WalletConnector";
 import BalanceViewer from "./BalanceViewer";
 
 function App() {
   const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    sdk.actions.ready();
+  }, []);
 
   return (
     <div className="app-root">
